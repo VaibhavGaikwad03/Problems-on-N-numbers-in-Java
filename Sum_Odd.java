ec0913b086c95ab5581c11a0db84f5200e162406
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-07-13 15:01:06 +0530

    Add files via upload

---
+++ Sum_Odd.java
@@ -0,0 +1,45 @@
+import java.lang.*;
+import java.util.*;
+import MyPackage.ArrayX;
+
+class MyArray extends ArrayX
+{
+    public MyArray(int iSize)
+    {
+        super(iSize);
+    }
+
+    public int SumOdd()
+    {
+        int iSumOdd = 0;
+
+        for(int i = 0; i < Arr.length; i++)
+        {
+            if((Arr[i] % 2) != 0)
+            {
+                iSumOdd = iSumOdd + Arr[i];
+            }
+        }
+        return iSumOdd;
+    }
+}
+
+class Sum_Odd
+{
+    public static void main(String arg[])
+    {
+        int iRet = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements : ");
+        int iSize = sobj.nextInt();
+
+        MyArray mobj = new MyArray(iSize);
+
+        mobj.Accept();
+        mobj.Display();
+        iRet = mobj.SumOdd();
+
+        System.out.println("Addition of Odd elements is : "+iRet);
+    }
+}
