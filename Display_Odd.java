adb7e04e62f11ffcd02e2a6c3392f59e8ff8b9bd
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2022-07-13 14:50:05 +0530

    Add files via upload

---
+++ Display_Odd.java
@@ -0,0 +1,41 @@
+import java.lang.*;
+import java.util.*;
+import MyPackage.ArrayX;
+
+class MyArray extends ArrayX
+{
+    public MyArray(int iSize)
+    {
+        super(iSize);
+    }
+
+    public void DisplayOdd()
+    {
+        System.out.println("Odd numbers are : ");
+        for(int i = 0; i < Arr.length; i++)
+        {
+            if((Arr[i] % 2) != 0)
+            {
+                System.out.println(Arr[i]);
+            }
+        }
+    }
+}
+
+class Display_Odd
+{
+    public static void main(String arg[])
+    {
+        int iRet = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements : ");
+        int iSize = sobj.nextInt();
+
+        MyArray mobj = new MyArray(iSize);
+
+        mobj.Accept();
+        mobj.Display();
+        mobj.DisplayOdd();
+    }
+}
